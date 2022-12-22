11b5de261ff2ead29b83a38cf72a2e2e223752b4
Aditya Shelke <adi.cs.night@gmail.com>
2022-12-22 16:07:36 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let svgpath = document.querySelector('path')
+let pathLength = svgpath.getTotalLength()
+
+
+
+// path.style.strokeDasharray = pathLength + ' ' + pathLength;
+// path.style.strokeDashoffset = pathLength + ' ' + pathLength;
+
+// window.addEventListener('click', () => {
+//     var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+//     var drawLength = pathLength * 100;
+//     path.style.strokeDashoffset = pathLength - drawLength;
+// })
+
+
