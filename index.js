431496ced6b7bea139301468e1b4ceb0ecbeb816
Aditya Shelke <adi.cs.night@gmail.com>
2022-12-22 17:37:51 +0530

    Transitions add

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 let svgpath = document.querySelector('path')
 let pathLength = svgpath.getTotalLength()
+let mapOpen = false;
 
+let menuButton = document.getElementsByClassName('menuButton')
+let menuText = document.getElementsByClassName('menuText')
+let lineSvg = document.getElementsByClassName('line-svg')
 
+menuButton[0].addEventListener("click", animate);
+
+function animate() {
+if(mapOpen){
+    mapOpen = true
+    menuText[0].classList.add('menuTextTransition')
+    menuText[1].classList.add('menuTextTransition')
+    menuText[2].classList.add('menuTextTransition')
+  
+    lineSvg[0].classList.add('line-svg-animate')
+  
+    setTimeout(function() {
+      lineSvg[0].classList.remove('line-svg')
+    }, 1000);
+}
+
+if(!mapOpen){
+    mapOpen = false
+    
+}
+  
+
+  
+}
 
 // path.style.strokeDasharray = pathLength + ' ' + pathLength;
 // path.style.strokeDashoffset = pathLength + ' ' + pathLength;
